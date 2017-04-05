3f6b0193534fd52b6220c0391a8c3d89d30f2f44
Matthew Harris <mharris@doublehorn.com>
2017-04-04 23:44:23 -0500

    submit pypi

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
   author = 'Matthew Harris',
   author_email = 'matt@x-qa.com',
   url = 'https://github.com/morissette/eventify',
-  download_url = 'https://github.com/morissette/eventify/tarball/0.0.1',
+  download_url = 'https://github.com/morissette/eventify/dist/0.0.1',
   keywords = ['event', 'event-driven', 'async', 'framework', 'producer', 'consumer', 'kinesis'],
   classifiers = [],
 )
