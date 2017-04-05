f938a8223fd6485888e9bf5f65638c2318022f24
Matthew Harris <mharris@doublehorn.com>
2017-04-04 22:36:48 -0500

    start writing meaningful tests

--- test/eventify.py
+++ test/eventify.py
@@ -5,5 +5,16 @@ import eventify
 import pytest
 
 class TestEventify:
-    def test_placeholder_one(self):
+
+    def setUp(self):
+        access_key = os.get('ACCESS_KEY')
+        secret_key = os.get('SECRET_KEY')
+        region_name = os.get('REGION_NAME')
+        self.ev = eventify.Eventify(access_key, secret_key, 'TestStream', region_name=region_name)
+
+    def test_create_topic(self):
+        response = self.ev.create_topic()
+        print response
+
+    def test_delete_topic(self):
         assert True == True
