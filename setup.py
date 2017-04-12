d35cd167ebe8dec3cd8f10c081e40099103cc613
Matthew Harris <mharris@doublehorn.com>
2017-04-11 20:36:58 -0500

    Update requirements

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import find_packages
 setup(
   name = 'eventify',
   packages = find_packages(),
-  version = '0.1.0',
+  version = '0.1.1',
   description = 'Event Driven Asynchronous Framework',
   author = 'Matthew Harris',
   author_email = 'matt@x-qa.com',
   url = 'https://github.com/morissette/eventify',
   download_url = 'https://github.com/morissette/eventify/dist/eventify-0.1.0.tar.gz',
-  keywords = ['event', 'event-driven', 'async', 'framework', 'producer', 'consumer', 'kinesis'],
+  keywords = ['event', 'event-driven', 'async', 'framework', 'producer', 'consumer', 'tornado'],
   classifiers = [],
 )
