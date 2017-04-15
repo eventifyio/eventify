26c7bdd504fdbfd878f55d6861516c7b8f1c1144
Matthew Harris <mharris@doublehorn.com>
2017-04-14 21:49:42 -0500

    0.1.6 -> 0.1.7

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ from setuptools import find_packages
 setup(
     name='eventify',
     packages=find_packages(),
-    version='0.1.6',
+    version='0.1.7',
     description='Event Driven Asynchronous Framework',
     author='Matthew Harris',
     author_email='matt@x-qa.com',
     url='https://github.com/morissette/eventify',
-    download_url='https://github.com/morissette/eventify/dist/eventify-0.1.6.tar.gz',
+    download_url='https://github.com/morissette/eventify/dist/eventify-0.1.7.tar.gz',
     keywords=['event', 'event-driven', 'async',
-              'framework', 'producer', 'consumer', 'tornado'],
+              'framework', 'producer', 'consumer'],
     classifiers=[],
     install_requires=[
-        'tornado',
-        'beanstalkt',
-        'rx'
+        'twisted',
+        'pyOpenSSL',
+        'autobahn'
     ]
 )
