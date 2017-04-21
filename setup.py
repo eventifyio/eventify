ac5516006b10ba35b6a579ea3e3137af6014d090
Matthew Harris <mharris@doublehorn.com>
2017-04-20 22:00:14 -0500

    0.1.8 -> 0.1.9 getting stabler!

--- setup.py
+++ setup.py
@@ -4,18 +4,19 @@ from setuptools import find_packages
 setup(
     name='eventify',
     packages=find_packages(),
-    version='0.1.8',
+    version='0.1.9',
     description='Event Driven Asynchronous Framework',
     author='Matthew Harris',
     author_email='matt@x-qa.com',
     url='https://github.com/morissette/eventify',
-    download_url='https://github.com/morissette/eventify/dist/eventify-0.1.8.tar.gz',
+    download_url='https://github.com/morissette/eventify/dist/eventify-0.1.9.tar.gz',
     keywords=['event', 'event-driven', 'async',
               'framework', 'producer', 'consumer'],
     classifiers=[],
     install_requires=[
         'twisted',
         'pyOpenSSL',
-        'autobahn'
+        'autobahn',
+        'service_identity'
     ]
 )
