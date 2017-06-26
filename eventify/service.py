0fdb04ab4104f674231b08db5968d18af0a02c92
Matthew Harris <mharris@doublehorn.com>
2017-06-26 16:48:16 -0500

    documentation of proxy decorator

--- eventify/service.py
+++ eventify/service.py
@@ -15,14 +15,29 @@ logger = logging.getLogger('eventify.service')
 
 
 def driver_function(func):
+    """
+    Decorator for proxying calls to drivers
+
+    Granted this is a little hacky it solves the problem where the
+    eventify project is not locked into a specific technology or
+    provider and allows for us to transparently change underlying
+    tech stack as necessary
+    """
     @wraps(func)
     def wrapper(*args, **kwargs):
         try:
+            # Get the function we want to execute on the driver
             function_name = func.__name__
+
+            # Import driver dynamically
             driver_to_load = 'eventify.drivers.' + args[0].driver
             logger.debug('loading driver %s', driver_to_load)
             driver = __import__(driver_to_load, globals(), locals(), ['create_service'])
+
+            # Create service on driver
             service = driver.create_service()
+
+            # Execute original function
             call_method = getattr(service, function_name)(args[0])
         except KeyError as error:
             logger.error(error)
@@ -36,9 +51,7 @@ class Service(Eventify):
     """
 
     @driver_function
-    def start(self):
-        pass
+    def start(self): pass
 
     @driver_function
-    def start_producer(self):
-        pass
+    def start_producer(self): pass
