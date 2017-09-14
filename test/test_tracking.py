e6f22a73937f58b889371fe43c7a0a585767d5de
Matthew Harris <mharris@doublehorn.com>
2017-09-14 14:12:39 -0500

    removed func from tests

--- test/test_tracking.py
+++ test/test_tracking.py
@@ -2,7 +2,7 @@ import json
 import unittest
 
 from eventify.event import Event
-from eventify.tracking import track_event, get_last_event
+from eventify.tracking import track_event
 from eventify.tracking.constants import EventState
 
 class TestTracking(unittest.TestCase):
@@ -10,4 +10,3 @@ class TestTracking(unittest.TestCase):
     @unittest.skip('redis not available via travisci')
     def test_get_last_event(self):
         service_name = 'example'
-        get_last_event(service_name)
